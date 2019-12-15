1e52b2af7e2ab8ef1cabdd7a5baeb032d75900a0
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2019-12-15 15:13:20 -0800

    register-added

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,63 @@
 const router = require('express').Router();
+const Userdb = require('../users/users-model')
+const bcrypt = require('bcrypt')
+
+
+
 
 router.post('/register', (req, res) => {
-  // implement registration
+
+    const user = req.body
+
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    user.password = hash;
+
+    if(user.username && user.password){
+      Userdb.add(user)
+        .then(user => {
+          res.status(201).json(user)
+        })
+        .catch(err => {
+          res.status(400).json(err)
+        })
+
+    } else {
+      console.log('not correct')
+    }
+
 });
 
+
+
+
+
+
 router.post('/login', (req, res) => {
-  // implement login
+  let {username, passsword} = req.body
+
+  Userdb.findBy({username})
+    .first()
+    .then(user => {
+      if(user && bcrypt.hashSync(user.password, 10)) {
+        const genToken(token)
+
+
+
+      }
+    }
+
+
+      
+    )
 });
+
+
+
+
+function genToken() {
+  
+}
+
+
 
 module.exports = router;
