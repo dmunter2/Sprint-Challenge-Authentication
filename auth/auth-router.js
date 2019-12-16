fe0a82c3bf24e2d2b316bab770ea60fc8f8f6f8d
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2019-12-15 16:35:14 -0800

    login working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const Userdb = require('../users/users-model')
 const bcrypt = require('bcrypt')
+const secrets = require('../secrets/secrets')
+const jwt = require('jsonwebtoken')
+
+
 
 
 
@@ -8,11 +12,9 @@ const bcrypt = require('bcrypt')
 router.post('/register', (req, res) => {
 
     const user = req.body
-
     const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
     user.password = hash;
 
-    if(user.username && user.password){
       Userdb.add(user)
         .then(user => {
           res.status(201).json(user)
@@ -21,42 +23,47 @@ router.post('/register', (req, res) => {
           res.status(400).json(err)
         })
 
-    } else {
-      console.log('not correct')
     }
-
-});
-
-
+)
 
 
 
 
 router.post('/login', (req, res) => {
-  let {username, passsword} = req.body
+  let { username, password } = req.body
 
   Userdb.findBy({username})
     .first()
     .then(user => {
-      if(user && bcrypt.hashSync(user.password, 10)) {
-        const genToken(token)
-
-
-
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user)
+        res.status(200).json({token: token})
+      } else {
+        res.status(400).json({error: 'invalid'})
       }
-    }
-
-
-      
-    )
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 });
 
 
 
 
-function genToken() {
-  
+
+function genToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username
+  }
+
+  const options = { expiresIn: '2h'}
+  const token = jwt.sign(payload, secrets.jwtSecret, options)
+  return token
 }
+
+
+
 
 
 
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,29 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken')
+const secrets = require('../secrets/secrets')
+const bcrypt = require('bcrypt')
+
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token = req.headers.authorization
+
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Cant pass - restricted Middleware " })
+      } else {
+        req.decodedJwt = decodedToken
+        next();
+
+      }
+    })
+
+  } else {
+    res.status(400).json({ message: "Cant recieve info" })
+
+  }
 };
